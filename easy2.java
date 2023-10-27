1ce892994fcc17225e69772b9790d7dd55cceb45
Helena <133242692+HellyGH@users.noreply.github.com>
2023-10-27 14:18:46 +0300

    Update easy2.java

--- easy2.java
+++ easy2.java
@@ -50,6 +50,28 @@ public class Main {
         odd();
 
       //zad2
+      static void reverse(){
+        int[] arr1 = new int[]{1, 2, 3, 4, 5,6,7,8,9,10};
+      
+        for (int i = 0; i < arr1.length / 2; i++) { //Разменя първото с последното и второто с предпоследното
+            int temp = arr1[i];
+            arr1[i] = arr1[arr1.length - 1 - i];
+            arr1[arr1.length - 1 - i] = temp;
+        }
+        System.out.println(Arrays.toString(arr1));
+
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(Arrays.toString(arr1));
+        reverse();
+
+
+    }
+//zad3
+      
       
 
 
