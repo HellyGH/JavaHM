f3c576ac8efd4949a95d3e86d5ece35c9c55f9a3
Helena <133242692+HellyGH@users.noreply.github.com>
2023-11-02 18:15:31 +0200

    Update easy3.java

--- easy3.java
+++ easy3.java
@@ -1,1 +1,48 @@
+//задачи от 1-3
+public class Main {
+    public static void main(String[] args) {
+        Person Helly = new Person();
+       // Helly.name="Helly";
+       // Helly.age=20;
+        Student student = new Student();
+       // student.grade = 5.50;
+
+        Helly.printDetails("Helly", 20, 5.50);
+    }
+}
+public class Person implements Printable{
+    String name;
+    int age;
+
+    void introduce(){
+        System.out.println("Hello this is "+name);
+        System.out.println("I'am" + age);
+    }
+     @Override
+    public void printDetails(String name, int age,double grade) {
+        System.out.println("Hello my name is" + name + "i am" + age + "and my grade is:" + grade);
+
+    }
+}
+public class Student extends Person{
+    double grade;
+
+    @Override
+    public void introduce() {
+        System.out.println("My grade is"+grade);
+    }
+
+    @Override
+    public void printDetails(String name, int age,double grade) {
+        super.printDetails(name, age,grade);
+    }
+}
+
+public interface Printable {
+    void printDetails(String name, int age,double grade);
+}
+
+//Задача 4
+
+
 
